e8abf1cb4ece59203aa883244c0cdd96f607d6ba
clepix21 <clement.lemaire2107@gmail.com>
2025-02-18 22:09:48 +0100

    Renommage de Form1 en main et réorganisation des fichiers
    Les fichiers `Form1.cs` et `Form1.Designer.cs` ont été remplacés par `fenetres\main.cs` et `fenetres\main.Designer.cs`. Une nouvelle ressource intégrée `fenetres\main.resx` a été ajoutée.
    
    L'instance de `Form1` dans `Program.cs` a été mise à jour pour utiliser `main`. La classe `main` hérite de `Form` et inclut un constructeur qui appelle `InitializeComponent()`.
    
    Le fichier `main.Designer.cs` définit l'interface utilisateur avec des contrôles tels qu'un `PictureBox` et des `Labels`.

--- fenetres/main.cs
+++ fenetres/main.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CodeBarre
 {
-    public partial class Form1: Form
+    public partial class main: Form
     {
-        public Form1()
+        public main()
         {
             InitializeComponent();
         }
